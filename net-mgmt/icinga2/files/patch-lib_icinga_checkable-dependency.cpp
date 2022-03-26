b8e9dfa93730a708769126e83906fd7c68970a4b
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2022-03-26 12:39:02 +0100

    net-mgmt/icinga2: update dependency patch

--- net-mgmt/icinga2/files/patch-lib_icinga_checkable-dependency.cpp
+++ net-mgmt/icinga2/files/patch-lib_icinga_checkable-dependency.cpp
@@ -1,15 +1,32 @@
---- lib/icinga/checkable-dependency.cpp.orig
-+++ lib/icinga/checkable-dependency.cpp
-@@ -83,6 +83,12 @@ bool Checkable::IsReachable(DependencyType dt, Dependency::Ptr *failedDependency
+--- lib/icinga/checkable-dependency.cpp.orig	2022-03-24 04:01:06.000000000 +0100
++++ lib/icinga/checkable-dependency.cpp	2022-03-26 12:34:31.639303000 +0100
+@@ -76,8 +76,15 @@
  
- 			if (failedDependency)
- 				*failedDependency = dep;
+ 	int countDeps = deps.size();
+ 	int countFailed = 0;
++	bool hasServiceDep = false;
+ 
+ 	for (const Dependency::Ptr& dep : deps) {
++		Host::Ptr    parentHost;
++		Service::Ptr parentService;
++		tie(parentHost, parentService) = GetHostService(dep->GetParent());
++		if (parentService)
++		  hasServiceDep = true;
 +
-+                        Host::Ptr    parentHost;
-+                        Service::Ptr parentService;
-+                        tie(parentHost, parentService) = GetHostService(dep->GetParent());
-+                        if (parentService)
-+                          return false;
- 		}
- 	}
+ 		if (!dep->IsAvailable(dt)) {
+ 			countFailed++;
  
+@@ -90,6 +97,13 @@
+ 	if (countDeps > 0 && countFailed == countDeps) {
+ 		Log(LogDebug, "Checkable")
+ 			<< "All dependencies have failed for checkable '" << GetName() << "': Marking as unreachable.";
++
++		return false;
++	}
++	/* If there are service dependencies, and any of dependencies failed, mark as unreachable. */
++	else if (hasServiceDep && countFailed > 0) {
++		Log(LogDebug, "Checkable")
++			<< "A dependency has failed for checkable with a service dependency '" << GetName() << "': Marking as unreachable.";
+ 
+ 		return false;
+ 	}
