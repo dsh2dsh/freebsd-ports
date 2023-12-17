5054d4d2a96e75cee14d7f47ea84ea3768643116
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2023-12-17 16:29:57 +0100

    deskutils/syncthingtray: fix fresh patch

--- deskutils/syncthingtray/files/patch-c++utilities_chrono_timespan.cpp
+++ deskutils/syncthingtray/files/patch-c++utilities_chrono_timespan.cpp
@@ -1,5 +1,5 @@
---- c++utilities/chrono/timespan.cpp.orig	2023-12-17 16:08:04.427911000 +0100
-+++ c++utilities/chrono/timespan.cpp	2023-12-17 16:08:21.333354000 +0100
+--- ./../c++utilities/chrono/timespan.cpp.orig	2023-12-17 16:08:04.427911000 +0100
++++ ./../c++utilities/chrono/timespan.cpp	2023-12-17 16:08:21.333354000 +0100
 @@ -5,6 +5,7 @@
  #include "../conversion/stringbuilder.h"
  #include "../conversion/stringconversion.h"
