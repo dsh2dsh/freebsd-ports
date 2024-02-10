ebfb8204f1e56d616fe0a4f01117ae486622998b
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2024-02-10 22:15:48 +0100

    lang/go122: fix patch

--- lang/go122/files/patch-src_cmd_go_internal_modload_vendor.go
+++ lang/go122/files/patch-src_cmd_go_internal_modload_vendor.go
@@ -1,11 +1,11 @@
---- src/cmd/go/internal/modload/vendor.go.orig	2023-08-02 13:51:49 UTC
-+++ src/cmd/go/internal/modload/vendor.go
-@@ -144,7 +144,7 @@ func checkVendorConsistency(index *modFileIndex, modFi
- 	readVendorList(MainModules.mustGetSingleMainModule())
- 
- 	pre114 := false
--	if gover.Compare(index.goVersion, "1.14") < 0 {
-+	if gover.Compare(index.goVersion, "1.14") < 0 || os.Getenv("GO_NO_VENDOR_CHECKS") == "1" {
- 		// Go versions before 1.14 did not include enough information in
- 		// vendor/modules.txt to check for consistency.
- 		// If we know that we're on an earlier version, relax the consistency check.
+--- src/cmd/go/internal/modload/vendor.go.orig	2024-02-10 22:04:57.320671000 +0100
++++ src/cmd/go/internal/modload/vendor.go	2024-02-10 22:14:29.877568000 +0100
+@@ -159,7 +159,7 @@
+ 			panic(fmt.Errorf("not in workspace mode but number of indexes is %v, not 1", len(indexes)))
+ 		}
+ 		index := indexes[0]
+-		if gover.Compare(index.goVersion, "1.14") < 0 {
++		if gover.Compare(index.goVersion, "1.14") < 0 || os.Getenv("GO_NO_VENDOR_CHECKS") == "1" {
+ 			// Go versions before 1.14 did not include enough information in
+ 			// vendor/modules.txt to check for consistency.
+ 			// If we know that we're on an earlier version, relax the consistency check.
