03acee12da7e46fae0c53e99a93edc0c0285127f
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2024-04-14 20:49:02 +0200

    www/firefox: add MOZ_OPTIONS+=--with-ccache
    See https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=224471

--- Mk/bsd.overlay.mk
+++ Mk/bsd.overlay.mk
@@ -1,11 +1,16 @@
 .if !defined(_DSH_OVERLAY_INCLUDED)
 _DSH_OVERLAY_INCLUDED=	yes
 
-.if ${.CURDIR:M*/www/ungoogled-chromium}
-.  if defined(WITH_CCACHE_BUILD) && !defined(NO_CCACHE)
- # https://chromium.googlesource.com/chromium/src.git/+/master/docs/ccache_mac.md#use-with-gn
- GN_ARGS+= cc_wrapper="${SETENV} CCACHE_SLOPPINESS=time_macros ${CCACHE_BIN}"
+.if defined(WITH_CCACHE_BUILD) && !defined(NO_CCACHE)
+.  if ${.CURDIR:M*/www/firefox}
+# https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=224471
+MOZ_OPTIONS+=	--with-ccache
+
+.  elif ${.CURDIR:M*/www/ungoogled-chromium}
+# https://chromium.googlesource.com/chromium/src.git/+/master/docs/ccache_mac.md#use-with-gn
+GN_ARGS+= cc_wrapper="${SETENV} CCACHE_SLOPPINESS=time_macros ${CCACHE_BIN}"
+
 .  endif
-.endif
+.endif # defined(WITH_CCACHE_BUILD) && !defined(NO_CCACHE)
 
 .endif # !defined(_DSH_OVERLAY_INCLUDED)
