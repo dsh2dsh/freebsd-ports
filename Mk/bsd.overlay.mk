c1c144c61878ddce44b1b9b413014717e688ccd5
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2024-04-20 19:06:12 +0200

    Mk/bsd.overlay.mk: remove CCACHE_SLOPPINESS=time_macros
    We already have time_macros in the ccache.conf.

--- Mk/bsd.overlay.mk
+++ Mk/bsd.overlay.mk
@@ -24,7 +24,7 @@ MOZ_OPTIONS+=	--with-ccache="${CCACHE_BIN}"
 
 .  elif ${.CURDIR:M*/www/ungoogled-chromium}
 # https://chromium.googlesource.com/chromium/src.git/+/master/docs/ccache_mac.md#use-with-gn
-GN_ARGS+=	cc_wrapper="${SETENV} CCACHE_SLOPPINESS=time_macros ${CCACHE_BIN}"
+GN_ARGS+=	cc_wrapper="${CCACHE_BIN}"
 
 .  endif
 .endif # defined(WITH_CCACHE_BUILD) && !defined(NO_CCACHE)
