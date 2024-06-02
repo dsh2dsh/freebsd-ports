bfc67f2779a3577ec0eb1dbde9277bcdd3dee0eb
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2024-06-02 19:56:13 +0200

    www/ungoogled-chromium: fix sqlite3_shim.c compilation error
    Applied patch from
    https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=279397#c7

--- www/ungoogled-chromium/files/patch-third__party_sqlite_src_amalgamation_sqlite3.c
+++ www/ungoogled-chromium/files/patch-third__party_sqlite_src_amalgamation_sqlite3.c
@@ -1,6 +1,16 @@
---- third_party/sqlite/src/amalgamation/sqlite3.c.orig	2024-05-23 20:04:36 UTC
+--- third_party/sqlite/src/amalgamation/sqlite3.c.orig	2024-05-23 22:40:42 UTC
 +++ third_party/sqlite/src/amalgamation/sqlite3.c
-@@ -44777,7 +44777,12 @@ static int unixRandomness(sqlite3_vfs *NotUsed, int nB
+@@ -20533,6 +20533,9 @@ SQLITE_PRIVATE int sqlite3HeapNearlyFull(void);
+ ** that deal with sqlite3StackAlloc() failures to be unreachable.
+ */
+ #ifdef SQLITE_USE_ALLOCA
++# if !defined(alloca) && defined(__FreeBSD__)
++#  define alloca(sz) __builtin_alloca(sz)
++# endif
+ # define sqlite3StackAllocRaw(D,N)   alloca(N)
+ # define sqlite3StackAllocRawNN(D,N) alloca(N)
+ # define sqlite3StackFree(D,P)
+@@ -44777,7 +44780,12 @@ static int unixRandomness(sqlite3_vfs *NotUsed, int nB
    memset(zBuf, 0, nBuf);
    randomnessPid = osGetpid(0);
  #if !defined(SQLITE_TEST) && !defined(SQLITE_OMIT_RANDOMNESS)
@@ -13,7 +23,7 @@
      int fd, got;
      fd = robust_open("/dev/urandom", O_RDONLY, 0);
      if( fd<0 ){
-@@ -44792,6 +44797,7 @@ static int unixRandomness(sqlite3_vfs *NotUsed, int nB
+@@ -44792,6 +44800,7 @@ static int unixRandomness(sqlite3_vfs *NotUsed, int nB
        robust_close(0, fd, __LINE__);
      }
    }
