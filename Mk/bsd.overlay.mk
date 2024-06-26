2c6a474345c147873f2c0a81efc62ecb010610eb
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2024-06-26 13:58:52 +0200

    bsd.overlay.mk: keep original SCCACHE_DIR
    We are overriding CCACHE_DIR for www/ungoogled-chromium
    
    CCACHE_DIR="/root/.ccache/chromium"
    
    and it changes SCCACHE_DIR too:
    
    SCCACHE_DIR="/root/.ccache/chromium/sccache"
    
    Actually it must be
    
    SCCACHE_DIR="/root/.ccache/sccache"

--- Mk/bsd.overlay.mk
+++ Mk/bsd.overlay.mk
@@ -1,12 +1,18 @@
 .if !defined(_DSH_OVERLAY_INCLUDED)
 _DSH_OVERLAY_INCLUDED=	yes
 
-.if defined(WITH_CCACHE_BUILD) && !defined(NO_CCACHE)
+.  if defined(SCCACHE_DIR)
+SCCACHE_DIR:=	${SCCACHE_DIR}
+.  elif defined(CCACHE_DIR)
+SCCACHE_DIR:=	${CCACHE_DIR}/sccache
+.  endif
+
+.  if defined(WITH_CCACHE_BUILD) && !defined(NO_CCACHE)
 # From bsd.ccache.mk, because it isn't included yet. The system includes
 # bsd.ccache.mk after bsd.overlay.mk.
-.  if defined(CCACHE_WRAPPER_PATH)
+.    if defined(CCACHE_WRAPPER_PATH)
 CCACHE_PKG_PREFIX=	${CCACHE_WRAPPER_PATH:C,/libexec/ccache$,,}
-.  endif
+.    endif
 CCACHE_PKG_PREFIX?=	${LOCALBASE}
 CCACHE_WRAPPER_PATH?=	${CCACHE_PKG_PREFIX}/libexec/ccache
 CCACHE_BIN?=		${CCACHE_PKG_PREFIX}/bin/ccache
@@ -24,23 +30,23 @@ CXXFLAGS+=	-fdebug-prefix-map=${WRKSRC}=.
 
 _USES_configure+=	250:ccache-update-links
 
-.  if ${.CURDIR:M*/www/firefox}
+.    if ${.CURDIR:M*/www/firefox}
 # https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=224471
 MOZ_OPTIONS+=	--with-ccache="${CCACHE_BIN}"
 
-.  elif ${.CURDIR:M*/www/ungoogled-chromium}
+.    elif ${.CURDIR:M*/www/ungoogled-chromium}
 # Use separate CCACHE_DIR
 CCACHE_DIR:=	${CCACHE_DIR}/chromium
 # https://chromium.googlesource.com/chromium/src.git/+/master/docs/ccache_mac.md#use-with-gn
 GN_ARGS+=	cc_wrapper="${CCACHE_BIN}"
 
-.  endif
-.endif # defined(WITH_CCACHE_BUILD) && !defined(NO_CCACHE)
+.    endif
+.  endif # defined(WITH_CCACHE_BUILD) && !defined(NO_CCACHE)
 
 # https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=274142
-.if ${.CURDIR:M*/www/firefox}
+.  if ${.CURDIR:M*/www/firefox}
 MAKE_ENV+=	CARGO_BUILD_JOBS=${MAKE_JOBS_NUMBER}
-.endif
+.  endif
 
 ccache-update-links:
 	@${ECHO_MSG} "==> ccache-update-links"
