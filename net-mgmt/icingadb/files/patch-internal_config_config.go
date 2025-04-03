70a73126f0a2bf61edee0b180b15c8dbf56f6807
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2025-04-03 17:05:53 +0200

    net-mgmt/icingadb: unbreak build

--- net-mgmt/icingadb/files/patch-internal_config_config.go
+++ net-mgmt/icingadb/files/patch-internal_config_config.go
@@ -1,11 +1,11 @@
---- internal/config/config.go.orig	1979-11-29 23:00:00 UTC
-+++ internal/config/config.go
-@@ -50,7 +50,7 @@ type Flags struct {
- 	// Version decides whether to just print the version and exit.
+--- internal/config/config.go.orig	2025-04-03 13:44:31.000000000 +0200
++++ internal/config/config.go	2025-04-03 17:03:33.450659000 +0200
+@@ -56,7 +56,7 @@
  	Version bool `long:"version" description:"print version and exit"`
- 	// Config is the path to the config file
--	Config string `short:"c" long:"config" description:"path to config file" required:"true" default:"/etc/icingadb/config.yml"`
-+	Config string `short:"c" long:"config" description:"path to config file" required:"true" default:"%%PREFIX%%/etc/icingadb/config.yml"`
- }
  
- // RetentionConfig defines configuration for history retention.
+ 	// Config is the path to the config file. If not provided, it defaults to DefaultConfigPath.
+-	Config string `short:"c" long:"config" description:"path to config file (default: /etc/icingadb/config.yml)"`
++	Config string `short:"c" long:"config" description:"path to config file (default: %%PREFIX%%/etc/icingadb/config.yml)"`
+ 	// default must be kept in sync with DefaultConfigPath.
+ 
+ 	// DatabaseAutoImport results in an initial schema check and update; mostly for containerized setups.
