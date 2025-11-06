831726a3b3559a4a2c764c86f2194d7500ca2bed
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2025-11-06 16:25:14 +0100

    net-p2p/transmission-components: Update to 4.1.0-beta.3
    https://github.com/transmission/transmission/releases/tag/4.1.0-beta.3

--- net-p2p/transmission-components/files/patch-cmake_FindUtfCpp.cmake
+++ net-p2p/transmission-components/files/patch-cmake_FindUtfCpp.cmake
@@ -1,8 +1,8 @@
---- cmake/FindUtfCpp.cmake.orig	2024-02-22 15:49:05 UTC
+--- cmake/FindUtfCpp.cmake.orig	2025-11-05 23:57:39 UTC
 +++ cmake/FindUtfCpp.cmake
 @@ -2,4 +2,4 @@ target_include_directories(utf8::cpp
  
  target_include_directories(utf8::cpp
      INTERFACE
--     ${CMAKE_CURRENT_LIST_DIR}/../third-party/utfcpp/source)
+-        ${TR_THIRD_PARTY_SOURCE_DIR}/utfcpp/source)
 +    	${CMAKE_INSTALL_PREFIX}/include/utf8cpp)
