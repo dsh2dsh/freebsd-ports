36a63049b180d947941a535219d4811e71cbcc07
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2026-09-14 18:06:13 +0200

    deskutils/syncthingtray: One more patch
    https://github.com/Martchus/syncthingtray/issues/459

--- deskutils/syncthingtray/files/patch-syncthingtray_plasmoid_lib_syncthingapplet.h
+++ deskutils/syncthingtray/files/patch-syncthingtray_plasmoid_lib_syncthingapplet.h
@@ -1,6 +1,16 @@
 --- syncthingtray/plasmoid/lib/syncthingapplet.h.orig	2026-09-13 15:05:04 UTC
 +++ syncthingtray/plasmoid/lib/syncthingapplet.h
-@@ -113,7 +113,9 @@ class SyncthingApplet : public Plasma::Applet { (publi
+@@ -83,7 +83,9 @@ class SyncthingApplet : public Plasma::Applet {
+             currentConnectionConfigIndexChanged)
+     Q_PROPERTY(int defaultTab READ defaultTab CONSTANT)
+     Q_PROPERTY(bool startStopEnabled READ isStartStopEnabled NOTIFY settingsChanged)
++#ifdef LIB_SYNCTHING_CONNECTOR_SUPPORT_SYSTEMD
+     Q_PROPERTY(QObject *startStopButtonTarget READ service CONSTANT)
++#endif
+     Q_PROPERTY(bool hasInternalErrors READ hasInternalErrors NOTIFY hasInternalErrorsChanged)
+     Q_PROPERTY(QSize size READ size WRITE setSize NOTIFY sizeChanged)
+     Q_PROPERTY(bool showTabTexts READ isShowingTabTexts WRITE setShowingTabTexts NOTIFY showTabTextsChanged)
+@@ -113,7 +115,9 @@ class SyncthingApplet : public Plasma::Applet {
  #if defined(SYNCTHINGWIDGETS_GUI_QTQUICK_MODE_DESKTOP)
      QtGui::QuickUI *quickUI() const;
  #endif
@@ -10,7 +20,7 @@
      QIcon syncthingIcon() const;
      QString incomingTraffic() const;
      bool hasIncomingTraffic() const;
-@@ -283,14 +285,12 @@ inline QtGui::QuickUI *SyncthingApplet::quickUI() cons
+@@ -283,14 +287,12 @@ inline QtGui::QuickUI *SyncthingApplet::quickUI() cons
  }
  #endif
  
