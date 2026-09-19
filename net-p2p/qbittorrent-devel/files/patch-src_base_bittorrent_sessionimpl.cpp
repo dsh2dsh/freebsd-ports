a98196c5c16fe5184eedd6b507eaa2bbf8031f7b
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2026-09-19 18:42:02 +0200

    net-p2p/qbittorrent-devel: Add debug "Caught exception"

---
+++ net-p2p/qbittorrent-devel/files/patch-src_base_bittorrent_sessionimpl.cpp
@@ -0,0 +1,15 @@
+--- src/base/bittorrent/sessionimpl.cpp.orig	2026-09-08 23:15:49 UTC
++++ src/base/bittorrent/sessionimpl.cpp
+@@ -6969,8 +6969,11 @@ void SessionImpl::updateTrackerEntryStatuses(lt::torre
+                 emit trackerEntryStatusesUpdated(torrent, trackers);
+             });
+         }
+-        catch (const std::exception &)
++        catch (const std::exception &e)
+         {
++          qWarning() << "Caught exception in " << Q_FUNC_INFO << ": " << QString::fromStdString(e.what());
++          LogMsg(tr("Caught exception: %s")
++            .arg(QString::fromStdString(err.what())), Log::CRITICAL);
+         }
+     });
+ }
