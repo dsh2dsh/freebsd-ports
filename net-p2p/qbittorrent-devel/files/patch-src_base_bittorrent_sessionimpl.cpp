d17e401ab3113dd6f269f4a663d0d7ffe857663b
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2026-09-19 19:11:19 +0200

    net-p2p/qbittorrent-devel: Add debug "Caught exception"

--- net-p2p/qbittorrent-devel/files/patch-src_base_bittorrent_sessionimpl.cpp
+++ net-p2p/qbittorrent-devel/files/patch-src_base_bittorrent_sessionimpl.cpp
@@ -1,6 +1,34 @@
 --- src/base/bittorrent/sessionimpl.cpp.orig	2026-09-08 23:15:49 UTC
 +++ src/base/bittorrent/sessionimpl.cpp
-@@ -6969,8 +6969,11 @@ void SessionImpl::updateTrackerEntryStatuses(lt::torre
+@@ -6768,7 +6768,12 @@ void SessionImpl::handleTorrentConflictAlert(const lt:
+             {
+                 torrentHandle.set_metadata(metadata->info_section());
+             }
+-            catch (const std::exception &) {}
++            catch (const std::exception &e)
++              {
++                qWarning() << "Caught exception in " << Q_FUNC_INFO << ": " << QString::fromStdString(e.what());
++                LogMsg(tr("Caught exception SessionImpl::handleTorrentConflictAlert: %s")
++                  .arg(QString::fromStdString(e.what())), Log::CRITICAL);
++              }
+         });
+     }
+     else if (torrent1)
+@@ -6782,7 +6787,12 @@ void SessionImpl::handleTorrentConflictAlert(const lt:
+             {
+                 torrentHandle.set_metadata(metadata->info_section());
+             }
+-            catch (const std::exception &) {}
++            catch (const std::exception &e)
++              {
++                qWarning() << "Caught exception in " << Q_FUNC_INFO << ": " << QString::fromStdString(e.what());
++                LogMsg(tr("Caught exception SessionImpl::handleTorrentConflictAlert: %s")
++                  .arg(QString::fromStdString(e.what())), Log::CRITICAL);
++              }
+         });
+     }
+     else
+@@ -6969,8 +6979,11 @@ void SessionImpl::updateTrackerEntryStatuses(lt::torre
                  emit trackerEntryStatusesUpdated(torrent, trackers);
              });
          }
@@ -8,7 +36,7 @@
 +        catch (const std::exception &e)
          {
 +          qWarning() << "Caught exception in " << Q_FUNC_INFO << ": " << QString::fromStdString(e.what());
-+          LogMsg(tr("Caught exception: %s")
++          LogMsg(tr("Caught exception SessionImpl::updateTrackerEntryStatuses: %s")
 +            .arg(QString::fromStdString(e.what())), Log::CRITICAL);
          }
      });
