0bea6d5d648c591c95666cbf608edf8e2fa8016e
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2026-09-21 18:14:14 +0200

    Delete custom net-p2p/transmission

--- net-p2p/transmission-components/files/patch-cmake_FindUtfCpp.cmake
+++
@@ -1,8 +0,0 @@
---- cmake/FindUtfCpp.cmake.orig	2025-11-05 23:57:39 UTC
-+++ cmake/FindUtfCpp.cmake
-@@ -2,4 +2,4 @@ target_include_directories(utf8::cpp
- 
- target_include_directories(utf8::cpp
-     INTERFACE
--        ${TR_THIRD_PARTY_SOURCE_DIR}/utfcpp/source)
-+    	${CMAKE_INSTALL_PREFIX}/include/utf8cpp)
